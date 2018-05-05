8d3a24a6badbacdfa34af8b3ea115c0005ca6439
AyaMahmoud <aya_conan_1992@yahoo.com>
2018-05-05 11:55:01 +0200

    Initial commit(Start Task)

---
+++ app/src/main/java/com/av/mojaztask/CheckFilterListActivity.java
@@ -0,0 +1,13 @@
+package com.av.mojaztask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckFilterListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_filter_list);
+    }
+}
